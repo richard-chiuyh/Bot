e34601aec8afaa293fcb1b7304c9a6cc533a6901
Richard Chiu <67826914+UsernameZRK@users.noreply.github.com>
2021-09-15 15:01:29 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,25 @@
+import os
+import discord
+from discord.ext import commands
+from discord.ext.commands import Context
+from cogs import tag, main
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+# from discord_slash import SlashCommand, SlashContext
+
+cogs = [tag,main]
+client = commands.Bot(command_prefix="!", intents = discord.Intents.all())
+# for cog in cogs:
+#     cog.setup(client)
+
+@client.command()
+async def reload(ctx: Context, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+    await ctx.send("Reloaded Cog")
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[0:-3]}')
+
+client.run("ODUxOTk3OTk2MzA3OTcyMTI3.YMAa9w.g5r-LxxZnBPh75tpRopmhuXYnho")
